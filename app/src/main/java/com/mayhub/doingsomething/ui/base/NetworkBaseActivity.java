7c59898b6b38fea104f5f27a0bf61282f7a3ad13
dhandzm@gmail.com <dhandzm@gmail.com>
2015-08-19 18:24:50 +0800

    import volley

--- app/src/main/java/com/mayhub/doingsomething/ui/base/NetworkBaseActivity.java
+++ app/src/main/java/com/mayhub/doingsomething/ui/base/NetworkBaseActivity.java
@@ -1,10 +1,17 @@
 package com.mayhub.doingsomething.ui.base;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.android.volley.toolbox.Volley;
 
 /**
  * Created by daihai on 2015/8/19.
  */
 public class NetworkBaseActivity extends Activity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Volley.newRequestQueue(this);
+    }
 }
