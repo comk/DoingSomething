ee939cc915915e749f58b2e9cb061b5227cafa47
dhandzm@gmail.com <dhandzm@gmail.com>
2015-08-20 17:40:07 +0800

    import greenDAO and initial db

--- app/src/main/java/com/mayhub/doingsomething/ui/base/NetworkBaseActivity.java
+++ app/src/main/java/com/mayhub/doingsomething/ui/base/NetworkBaseActivity.java
@@ -3,15 +3,26 @@ package com.mayhub.doingsomething.ui.base;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
+import com.mayhub.doingsomething.net.VolleyUtils;
 
 /**
  * Created by daihai on 2015/8/19.
  */
 public class NetworkBaseActivity extends Activity {
+
+    public static final String TAG = NetworkBaseActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Volley.newRequestQueue(this);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        VolleyUtils.getRequestQueueInstance(getApplicationContext()).cancelAll(TAG);
     }
 }
