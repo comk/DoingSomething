8ce092d4337c2158d066adff10f6a7884f96d20b
dhandzm@gmail.com <dhandzm@gmail.com>
2015-08-31 10:48:41 +0800

    Image Viewer cache feature

--- app/src/main/java/com/mayhub/doingsomething/ui/adapter/ImageViewerAdapter.java
+++ app/src/main/java/com/mayhub/doingsomething/ui/adapter/ImageViewerAdapter.java
@@ -3,7 +3,7 @@ package com.mayhub.doingsomething.ui.adapter;
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 import java.util.Stack;
@@ -14,7 +14,9 @@ import java.util.Stack;
 public class ImageViewerAdapter extends PagerAdapter {
 
     private ArrayList<String> data = new ArrayList<>();
-
+    /**
+     * enable pagerAdapter's view cache feature
+     */
     private Stack<View> cacheViews = new Stack<>();
 
     public ImageViewerAdapter(){
@@ -39,11 +41,50 @@ public class ImageViewerAdapter extends PagerAdapter {
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
+        //check whether the cache stack is empty, if it is empty,we need to create from layout,otherwise we use the cache one;
+        if(cacheViews.isEmpty()){
+
+        }else{
+
+        }
         return super.instantiateItem(container, position);
+    }
+
+    private void bindView(){
+
     }
 
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
-        super.destroyItem(container, position, object);
+        //save view cache into stack ,when instantiate item check stack whether has cache first
+        cacheViews.push((View) object);
+        container.removeView((View) object);
     }
+
+    /**
+     * ViewHolder
+     */
+    public static class ImageViewerHolder{
+        /**
+         * The Image
+         */
+        public ImageView iv_item;
+        /**
+         * delete image option
+         */
+        public ImageView iv_delete;
+        /**
+         * save image option
+         */
+        public ImageView iv_save;
+        /**
+         * comment image option
+         */
+        public ImageView iv_comment;
+        /**
+         * like image option
+         */
+        public ImageView iv_like;
+    }
+
 }
